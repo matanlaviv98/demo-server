4342b524ef3f8d05e86a63d2e8a4fe723e8e8178
matanlaviv98 <matanlaviv98@gmail.com>
2022-09-01 21:18:47 +0300

    hardcoded run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
